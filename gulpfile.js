54fb03a0f54ccebb90590e93783f03d3a2ea07bf
Austin <austin@thisisvisceral.com>
2017-02-20 16:01:55 -0800

    Restructuring project folders for public use

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('sass', function() {
 // Watchers
 gulp.task('watch', function() {
   gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('index.html', browserSync.reload);
   gulp.watch('src/js/**/*.js', browserSync.reload);
 })
 
